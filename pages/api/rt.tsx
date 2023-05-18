714235f6cf9762b33cd55f59c1aba168f7a44f34
silentstorm902 <jozsefkiss90@gmail.com>
2023-05-18 21:54:03 +0200

    finished auth

--- pages/api/rt.tsx
+++ pages/api/rt.tsx
@@ -4,7 +4,7 @@ import { getSession } from 'next-auth/react';
 export default async function handler(req, res) {
     const session =await getSession({req});
     
-    if(!session|| session.user.role != 'user') {
+    if(!session || session.user.role != 'user') {
         res.status(403).json({ message: 'unauthorized'});
     }
     connectToDb()
