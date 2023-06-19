8f0974db1918c99ac21d162f5d69f6beebfbf1ab
silentstorm902 <jozsefkiss90@gmail.com>
2023-06-19 17:21:05 +0200

    tying new urls

--- pages/login.tsx
+++ pages/login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
 
   async function handleFacebookSignin() {
     const result = await signIn('facebook', {
-      callbackUrl: 'https://platform-app.herokuapp.com/api/auth/callback/facebook'
+      callbackUrl: 'https://platform-app.herokuapp.com'
     });
      if (result?.error) {
      console.error('Error signing in:', result.error);
