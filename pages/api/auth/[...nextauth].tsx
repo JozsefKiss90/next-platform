8832b7de2e1762e4a11d303878513cb1ab1bd3db
silentstorm902 <jozsefkiss90@gmail.com>
2023-08-14 12:54:40 +0200

    added NEXTAUTH_SECRET to oauth

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -14,6 +14,10 @@ enum Role {
   admin = "admin",
 }
 
+if (!process.env.NEXTAUTH_SECRET) {
+  throw new Error("Please provide process.env.NEXTAUTH_SECRET");
+}
+
 const providers = [];
 
 if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
